d92e3e0e527f1fb15889729456a4ccae73437f20
David Lukens <davidqcy02@gmail.com>
2025-06-28 20:40:19 -0500

    add test for nested routing

--- server/api/routing_test.go
+++ server/api/routing_test.go
@@ -55,3 +55,56 @@ func Test_RouterAcceptsCorrectMethods(t *testing.T) {
 		t.Run(name, createTestCase(method))
 	}
 }
+
+func Test_RouterNestingWithAndWithoutGroups(t *testing.T) {
+	cases := []struct {
+		route    string
+		expected string
+	}{
+		{"/api/a", "a"},
+		{"/api/a/a", "aa"},
+		{"/api/b/b", "b"},
+		{"/api/b/b/b", "bb"},
+	}
+
+	createTestCase := func(route string, expected string) func(t *testing.T) {
+		return func(t *testing.T) {
+			router := NewRouter("/api")
+			group := router.RouteGroup("/b")
+
+			router.Get("/a", func(request *RequestReader) ResponseWriter {
+				return SendOk("a")
+			})
+
+			router.Get("/a/a", func(request *RequestReader) ResponseWriter {
+				return SendOk("aa")
+			})
+
+			group.Get("/b", func(request *RequestReader) ResponseWriter {
+				return SendOk("b")
+			})
+
+			group.Get("/b/b", func(request *RequestReader) ResponseWriter {
+				return SendOk("bb")
+			})
+
+			url := fmt.Sprintf("http://localhost:8080%s", route)
+			req := httptest.NewRequest("GET", url, nil)
+
+			w := httptest.NewRecorder()
+			router.mux.ServeHTTP(w, req)
+
+			resp := w.Result()
+
+			body, err := io.ReadAll(resp.Body)
+			assert.NoError(t, err)
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+			assert.Equal(t, expected, string(body))
+		}
+	}
+
+	for _, c := range cases {
+		name := fmt.Sprintf("Route %s", c.route)
+		t.Run(name, createTestCase(c.route, c.expected))
+	}
+}
